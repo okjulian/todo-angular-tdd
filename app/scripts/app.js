d6d14c5cd665d1684934f6db05d2bfcc982bd2c4
Julian <julian@julian-laptop.(none)>
2013-02-07 15:39:15 -0300

    yeoman init angular with bootstrap

---
+++ app/scripts/app.js
@@ -0,0 +1,13 @@
+'use strict';
+
+var todoApp = angular.module('todoApp', [])
+  .config(['$routeProvider', function($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  }]);
