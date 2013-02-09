85cc81c6198de5bb1d05db2089ba053e0feea3ca
JulianMayorga <mayorga.julian@gmail.com>
2013-02-09 20:21:01 -0300

    Initial dist subtree commit

---
+++ dist/scripts/controllers/2ef2119c.main.js
@@ -0,0 +1,19 @@
+'use strict';
+
+todoApp.controller('MainCtrl', function($scope) {
+
+  $scope.items = [];
+
+  $scope.add = function() {
+    if ($scope.newText !== undefined && $scope.newText !== '') {
+      $scope.items.push({text: $scope.newText, done: false});
+      $scope.newText = '';
+    };
+  };
+
+  $scope.remove = function(item) {
+    var index = $scope.items.indexOf(item);
+    $scope.items.splice(index, 1);
+  };
+
+});
